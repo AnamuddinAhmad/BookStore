804974d78fbee484326f6f5b434184c1c2f1b98c
anamuddinahmad <anamuddinahmad0786@gmail.com>
2024-11-07 22:48:19 +0530

    Initial Commite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div className='h-screen w-full bg-zinc-800 text-white'>
+
+   <h1>Its Working</h1>
+   </div>
+  )
+}
+
+export default App
