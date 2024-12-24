607bfde56264aed4ae0714e918519c8897513066
anamuddinahmad <anamuddinahmad0786@gmail.com>
2024-12-24 14:03:07 +0530

    Setup Tailwind

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="bg-black min-h-screen px-4 py-4">
+      <div className="text-3xl p-4 font-bold underline w-full h-44 bg-[#CDF3E2] rounded-md  shadow-emerald-700 shadow-md  border-2 border-emerald-500 flex justify-between">
+        <h1 className="text-black">Book Store!</h1>
+        <p className="shadow-md shadow-emerald-700 border-2 h-fit border-emerald-700 bg-green-400 p-3 text-sm w-fit rounded-full bg-opacity-30 ">26</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
