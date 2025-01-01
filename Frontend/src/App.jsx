608f182fbfa78e3e20e5dbf615e8f69d9b7f78b0
anamuddinahmad <anamuddinahmad0786@gmail.com>
2025-01-01 21:44:18 +0530

    Changes in App

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,13 +3,7 @@ import Login from "./Auth/Login";
 
 const App = () => {
   return (
-    <div className="bg-gray-100 min-h-screen px-4 py-4">
-      <div className="text-3xl p-4 font-bold underline w-full  bg-[#CDF3E2] rounded-md  shadow-emerald-700 shadow-md  border-2 border-emerald-500 flex justify-between items-center">
-        <h1 className="text-emerald-700">Book Store!</h1>
-       
-      <Login />
-      </div>
-    </div>
+    <h1>Hello</h1>
   );
 };
 
